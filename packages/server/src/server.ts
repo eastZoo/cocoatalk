ec916002bc918f7c6b95c12b62cbb3d270325430
eastzoo <dongju010@kakao.com>
2023-02-18 13:51:04 +0900

    ADD: 세션 설정

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,9 +1,22 @@
 import express, { Application } from "express";
+import session from "express-session";
+
 import sequelize from "./sequelize";
 import routes from "./routes";
 
+const FileStore = require("session-file-store")(session);
+
 const app: Application = express();
 
+const sessionMiddleware = session({
+  secret: "kakaonibs",
+  saveUninitialized: true,
+  cookie: { secure: false }, // one day
+  resave: false,
+  store: new FileStore(),
+});
+
+app.use(sessionMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
