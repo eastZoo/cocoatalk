7a7356f9fced01640497aa7ac396ed2e72747f80
eastzoo <dongju010@kakao.com>
2023-02-22 16:57:54 +0900

    ADD : 라우터 설정

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Lobby from "./pages/Lobby";
+import RoomList from "./pages/RoomList";
+import RoomDetail from "./pages/RoomDetail";
+import Friends from "./pages/Friends";
+import Setting from "./pages/SeeMore";
 
 function App() {
-  return <div className="App">Hello cocoa!!</div>;
+  return (
+    <Routes>
+      <Route index element={<Lobby />} />
+      <Route path="/friends" element={<Friends />} />
+      <Route path="/rooms" element={<RoomList />} />
+      <Route path="/rooms/:roomId" element={<RoomDetail />} />
+      <Route path="/more" element={<Setting />} />
+    </Routes>
+  );
 }
 
 export default App;
